478ddd06d7f2066a13a0ee27d16b38b8c7efcdfd
Smit Desai <smit.desai16@gmail.com>
2020-06-20 17:10:51 -0500

    Adding close button in Second Activity

--- app/src/main/java/me/creatorguy/androidactivityexplicitandimplicitintent/SecondActivity.java
+++ app/src/main/java/me/creatorguy/androidactivityexplicitandimplicitintent/SecondActivity.java
@@ -4,26 +4,39 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class SecondActivity extends AppCompatActivity {
-
-    private static TextView tvSecondBanner;
+public class SecondActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        tvSecondBanner = findViewById(R.id.tvSecondBanner);
+        TextView tvSecondBanner = findViewById(R.id.tvSecondBanner);
+        Button btnSecondClose = findViewById(R.id.btnSecondClose);
 
+        btnSecondClose.setOnClickListener(this);
 
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
 
         if(intent.hasExtra("data")) {
             tvSecondBanner.setText(bundle.getString("data"));
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnSecondClose:
+                finish();
+                break;
+
+            default:
+                break;
         }
     }
 }
